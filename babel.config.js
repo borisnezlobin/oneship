e2052ee7c5cbbdc588bd539d93de157d9270301a
Rand0mLetters <boris.nezlobin@gmail.com>
2023-03-10 20:56:37 -0800

    when your brain don't work like it used to before actual skill issue though double clicked the button for "don't search in ignore files" so I thought it was searching node_modules it wasn't and then twitter was the ultimate source of truth ez

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
 module.exports = function (api) {
   api.cache(true);
     return {
-      presets: ['babel-preset-expo'],
+      presets: [
+        'babel-preset-expo',
+        ['module:metro-react-native-babel-preset', {
+          unstable_disableES6Transforms: true
+        }]
+      ],
       plugins: [
         '@babel/plugin-proposal-export-namespace-from',
         'react-native-reanimated/plugin',
