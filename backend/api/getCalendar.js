f1d177bc708f2bffeffc5defce9793722e9cf875
Rand0mLetters <me@borisnezlobin.com>
2023-08-09 18:50:23 -0700

    update alts

--- backend/api/getCalendar.js
+++ backend/api/getCalendar.js
@@ -2,23 +2,6 @@ import fetch from "node-fetch";
 
 const alternates = [
     {
-        date: "20230809",
-        schedule: [
-            { name: "0 Period", start: "7:50", end: "8:50", },
-            { name: "Advisory 9+11", start: "9:00", end: "9:15" },
-            { name: "Advisory 10+12", start: "9:20", end: "9:30", },
-            { name: "1st Period", start: "9:45", end: "10:25" },
-            { name: "Brunch", start: "10:25", end: "10:40" },
-            { name: "2nd Period", start: "10:40", end: "11:20" },
-            { name: "3rd Period", start: "11:30", end: "12:10" },
-            { name: "Lunch", start: "12:10", end: "12:50" },
-            { name: "4th Period", start: "12:50", end: "1:30" },
-            { name: "5th Period", start: "1:40", end: "2:20" },
-            { name: "6th Period", start: "2:30", end: "3:10" },
-            { name: "7th Period", start: "3:20", end: "4:00" },
-        ],
-    },
-    {
         date: "20230810",
         schedule: [
             { name: "0 Period", start: "7:50", end: "8:50", },
@@ -26,8 +9,8 @@ const alternates = [
             { name: "Brunch", start: "10:00", end: "10:15" },
             { name: "2nd Period", start: "10:15", end: "11:15" },
             { name: "Lunch", start: "11:15", end: "11:55" },
-            { name: "Advisory 9+11", start: "11:55", end: "12:40" },
-            { name: "Class Meetings 9+11", start: "12:50", end: "1:35" },
+            { name: "Advisory 9+11/CM 10+12", start: "11:55", end: "12:40" },
+            { name: "Class Meetings 9+11/Adv 10+12", start: "12:50", end: "1:35" },
             { name: "3rd Period", start: "1:45", end: "2:45" },
             { name: "4th Period", start: "2:55", end: "3:55" },
         ],
